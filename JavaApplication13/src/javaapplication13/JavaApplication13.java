f637ddc00b91aed1ccdbe52a763277f98a09157e
musariul30 <musariul30@student.uns.ac.id>
2023-06-21 14:07:06 +0700

    Update JavaApplication13.java

--- JavaApplication13/src/javaapplication13/JavaApplication13.java
+++ JavaApplication13/src/javaapplication13/JavaApplication13.java
@@ -14,7 +14,7 @@ public class JavaApplication13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello");   
+        System.out.println("Hello Word");   
     }
     
 }
