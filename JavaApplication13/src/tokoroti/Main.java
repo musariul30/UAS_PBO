00bb69c9b14ef884563ad03898d8ac2a066e397f
musariul30 <musariul30@student.uns.ac.id>
2023-06-21 14:36:41 +0700

    ganti nama

--- JavaApplication13/src/tokoroti/Main.java
+++ JavaApplication13/src/tokoroti/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication13;
+package tokoroti;
 
 /**
  *
  * @author Musariul
  */
-public class JavaApplication13 {
+public class Main {
 
     /**
      * @param args the command line arguments
--- JavaApplication13/src/tokoroti/NewClass3.java
+++ JavaApplication13/src/tokoroti/NewClass3.java
@@ -2,12 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package javaapplication13;
+package tokoroti;
 
 /**
  *
  * @author LENOVO
  */
 public class NewClass3 {
+    //isoh niff
     
 }
