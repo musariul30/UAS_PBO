6b0b281ed37a12231319a7ed06af52a057bdb651
arnellahayya <arnellahayya25@gmail.com>
2023-06-25 06:31:05 +0700

    Alhamdullah ketemu salahnya
    - pembulatan harga jual
    - penambahan harga modal roti di harga jual varian roti

--- BakeryShop/src/bakery/HargaJualVarianPizzaPcs.java
+++ BakeryShop/src/bakery/HargaJualVarianPizzaPcs.java
@@ -11,11 +11,12 @@ package bakery;
 public class HargaJualVarianPizzaPcs extends ModalPizzaPerPcs {
     
     // Hitung harga jual varian roti pizza per pcs
-    double hargaJualPizzaV1 = ( (this.hargaPerGramKeju * 30) + (this.hargaPerGramSosis * 50) + (this.hargaPerGramBeef * 50) + (this.hargaPerGramBombay * 30) ) * 140/100 ;
-
+    double hargaJualPizzaV1 = ( this.totalModalPcsPizza +(this.hargaPerGramKeju * 30) + (this.hargaPerGramSosis * 50) + (this.hargaPerGramBeef * 50) + (this.hargaPerGramBombay * 30) ) * 140/100 ;
+    int hargaJualPizzaV1Bulat = (int) Math.ceil(hargaJualPizzaV1);
+    
     // Print harga
     public void printHargaPizza(){
-        System.out.println("Harga Pizza Varian 1 Rp: " + this.hargaJualPizzaV1);
+        System.out.println("Harga Pizza Varian 1      Rp: " + this.hargaJualPizzaV1Bulat);
     }
     
 }
--- BakeryShop/src/bakery/HargaJualVarianRotiTawarPcs.java
+++ BakeryShop/src/bakery/HargaJualVarianRotiTawarPcs.java
@@ -11,14 +11,16 @@ package bakery;
 public class HargaJualVarianRotiTawarPcs extends ModalRotiTawarPerPcs {
     
     // Hitung harga jual varian roti tawar per pcs
-    double hargaJualRotiTawarV1 = ( (this.hargaPerGramCoklat * 80) ) * 140/100 ;
-    double hargaJualRotiTawarV2 = ( (this.hargaPerGramKeju * 80) ) * 140/100 ;
-
+    double hargaJualRotiTawarV1 = ( this.totalModalPcsRotiTawar +(this.hargaPerGramCoklat * 80) ) * 140/100 ;
+    int hargaJualRotiTawarV1Bulat = (int) Math.ceil(hargaJualRotiTawarV1);
+    double hargaJualRotiTawarV2 = ( this.totalModalPcsRotiTawar +(this.hargaPerGramKeju * 80) ) * 140/100 ;
+    int hargaJualRotiTawarV2Bulat = (int) Math.ceil(hargaJualRotiTawarV2);
+    
     // Print harga
     public void printHargaTawarV1(){
-        System.out.println("Harga Roti Tawar Varian 1 Rp: " + this.hargaJualRotiTawarV1);
+        System.out.println("Harga Roti Tawar Varian 1 Rp: " + this.hargaJualRotiTawarV1Bulat);
     }
      public void printHargaTawarV2(){
-        System.out.println("Harga Roti Tawar Varian 2 Rp: " + this.hargaJualRotiTawarV2);
+        System.out.println("Harga Roti Tawar Varian 2 Rp: " + this.hargaJualRotiTawarV2Bulat);
     }
 }
