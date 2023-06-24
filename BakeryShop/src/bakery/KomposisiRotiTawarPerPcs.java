bf435ce6235397f052cc8a07684e0ea07458f9a8
arnellahayya <arnellahayya25@gmail.com>
2023-06-24 22:58:44 +0700

    Create KomposisiRotiTawarPerPcs.java

---
+++ BakeryShop/src/bakery/KomposisiRotiTawarPerPcs.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bakery;
+
+/**
+ *
+ * @author Arnella
+ */
+public class KomposisiRotiTawarPerPcs extends KomposisiRotiTawar {
+    // Hitung bahan per pcs rotiTawar
+    double teriguPcsRotiTawar = this.rotiTawarTerigu / this.rotiTawarPcs;
+    double gulaPcsRotiTawar = this.rotiTawarGula / this.rotiTawarPcs;
+    double butterPcsRotiTawar = this.rotiTawarButter / this.rotiTawarPcs;
+    double ragiPcsRotiTawar = this.rotiTawarRagi / this.rotiTawarPcs;
+    double susuBubukPcsRotiTawar = this.rotiTawarSusuBubuk / this.rotiTawarPcs;
+    double susuCairPcsRotiTawar = this.rotiTawarSusuCair / this.rotiTawarPcs;
+    double TelurPcsRotiTawar = this.rotiTawarTelur / this.rotiTawarPcs;
+    double esPcsRotiTawar = this.rotiTawarEs / this.rotiTawarPcs;
+    
+}
