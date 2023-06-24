76c618eec8bf8990238e2e1b64757c525325228a
musariul30 <musariul30@student.uns.ac.id>
2023-06-24 20:48:16 +0700

    Update KomposisiPizzaPerPcs.java

--- BakeryShop/src/bakery/KomposisiPizzaPerPcs.java
+++ BakeryShop/src/bakery/KomposisiPizzaPerPcs.java
@@ -12,6 +12,12 @@ public class KomposisiPizzaPerPcs extends KomposisiPizza{
     
     // Hitung bahan per pcs pizza
     double teriguPcsPizza = this.pizzaTerigu / this.pizzaPcs;
-    
+    double gulaPcsPizza = this.pizzaGula / this.pizzaPcs;
+    double butterPcsPizza = this.pizzaButter / this.pizzaPcs;
+    double ragiPcsPizza = this.pizzaRagi / this.pizzaPcs;
+    double susuBubukPcsPizza = this.pizzaSusuBubuk / this.pizzaPcs;
+    double susuCairPcsPizza = this.pizzaSusuCair / this.pizzaPcs;
+    double TelurPcsPizza = this.pizzaTelur / this.pizzaPcs;
+    double esPcsPizza = this.pizzaEs / this.pizzaPcs;
     
 }
