8834bbdf7a62594aa60c264b872372f0c7980268
arnellahayya <arnellahayya25@gmail.com>
2023-06-26 15:48:36 +0700

    Buat Interface Hitung Bahan

---
+++ BakeryShop/src/bakery/InterfaceHitungBahan.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package bakery;
+
+/**
+ *
+ * @author Arnella
+ */
+public interface InterfaceHitungBahan {
+    
+    public abstract double bahanTerigu();
+    public abstract double bahanGula();
+    public abstract double bahanButter();
+    public abstract double bahanRagi();
+    public abstract double bahanSusuBubuk();
+    public abstract double bahanSusuCair();
+    public abstract double bahanTelur();
+    public abstract double bahanEs();
+    public abstract double bahanKeju();
+    public abstract double bahanCoklat();
+    public abstract double bahanVanila();
+    public abstract double bahanRedBean();
+    public abstract double bahanSosis();
+    public abstract double bahanBeef();
+    public abstract double bahanBombay();
+}
