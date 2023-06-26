83c32a5fa8db4194668cd33ff344490cc7275a8b
arnellahayya <arnellahayya25@gmail.com>
2023-06-26 16:08:59 +0700

    buat class bahan roti tawar dan pizza

---
+++ BakeryShop/src/bakery/BahanRotiTawarV1.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bakery;
+
+/**
+ *
+ * @author Arnella
+ */
+public class BahanRotiTawarV1 extends RotiTawar implements InterfaceHitungBahan{
+
+    public int orderan;
+    
+    @Override
+    public double bahanTerigu() {
+        double hitung = this.teriguPcsRotiTawar * orderan;
+        return hitung;
+    }
+
+    @Override
+    public double bahanGula() {
+        double hitung = this.gulaPcsRotiTawar * orderan;
+        return hitung;
+    }
+
+    @Override
+    public double bahanButter() {
+        double hitung = this.butterPcsRotiTawar * orderan;
+        return hitung;
+    }
+
+    @Override
+    public double bahanRagi() {
+        double hitung = this.ragiPcsRotiTawar * orderan;
+        return hitung;
+    }
+
+    @Override
+    public double bahanSusuBubuk() {
+        double hitung = this.susuBubukPcsRotiTawar * orderan; 
+        return hitung;        
+    }
+
+    @Override
+    public double bahanSusuCair() {
+        double hitung = this.susuCairPcsRotiTawar * orderan;
+        return hitung;
+    }
+
+    @Override
+    public double bahanTelur() {
+        double hitung = this.TelurPcsRotiTawar * orderan;
+        return hitung;
+                
+    }
+
+    @Override
+    public double bahanEs() {
+        double hitung = this.esPcsRotiTawar * orderan;
+        return hitung;
+    }
+
+    @Override
+    public double bahanKeju() {
+        return 0;
+    }
+
+    @Override
+    public double bahanCoklat() {
+        double hitung = 80 * orderan;
+        return  hitung;
+    }
+
+    @Override
+    public double bahanVanila() {
+        return 0;
+    }
+
+    @Override
+    public double bahanRedBean() {
+        return 0;
+    }
+
+    @Override
+    public double bahanSosis() {
+        return 0;
+    }
+
+    @Override
+    public double bahanBeef() {
+        return 0;
+    }
+
+    @Override
+    public double bahanBombay() {
+        return 0;
+    }
+}
---
+++ BakeryShop/src/bakery/BahanRotiTawarV2.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bakery;
+
+/**
+ *
+ * @author Arnella
+ */
+public class BahanRotiTawarV2 extends RotiTawar implements InterfaceHitungBahan{
+
+    @Override
+    public double bahanTerigu() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanGula() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanButter() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanRagi() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanSusuBubuk() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanSusuCair() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanTelur() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanEs() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanKeju() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanCoklat() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanVanila() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanRedBean() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanSosis() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanBeef() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double bahanBombay() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
