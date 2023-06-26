bf87c82e545f85a0337b4fe4416fda814fa2f7e8
RuthRadekta <ruthradektal@gmail.com>
2023-06-26 18:10:51 +0700

    Up final Main

--- BakeryShop/src/bakery/Main.java
+++ BakeryShop/src/bakery/Main.java
@@ -28,11 +28,8 @@ public class Main {
         t.printHargaTawarV2();
         Pizza p = new Pizza();
         p.printHargaPizza();
-
-        System.out.println("===================================");
-        System.out.println("     BAHAN YANG DIBUTUHKAN");
-        System.out.println("===================================");   
     
+        //Membuat Objek baru
         BahanRotiManisV1 rmV1 = new BahanRotiManisV1();
         BahanRotiManisV2 rmV2 = new BahanRotiManisV2();
         BahanRotiManisV1 rmV3 = new BahanRotiManisV1();
@@ -40,6 +37,7 @@ public class Main {
         BahanRotiTawarV1 rtv2 = new BahanRotiTawarV1();
         BahanPizza pv1 = new BahanPizza();
         
+        //Daftar orderan per varian
         rmV1.orderan = 50;
         rmV2.orderan = 100;
         rmV3.orderan = 70;
@@ -47,6 +45,7 @@ public class Main {
         rtv2.orderan = 15;
         pv1.orderan = 10;
         
+        //Total Berat Bahan yang dibutuhkan
         double hitungTerigu = Math.ceil(rmV1.bahanTerigu() + rmV2.bahanTerigu()+ rmV3.bahanTerigu() + rtv1.bahanTerigu()+ rtv2.bahanTerigu()+ pv1.bahanTerigu());
         double hitungGula = Math.ceil(rmV1.bahanGula() + rmV2.bahanGula()+ rmV3.bahanGula() + rtv1.bahanGula()+ rtv2.bahanGula()+ pv1.bahanGula());
         double hitungButter = Math.ceil(rmV1.bahanButter() + rmV2.bahanButter()+ rmV3.bahanButter() + rtv1.bahanButter()+ rtv2.bahanButter()+ pv1.bahanButter());
@@ -56,7 +55,7 @@ public class Main {
         double hitungTelur = Math.ceil(rmV1.bahanTelur() + rmV2.bahanTelur()+ rmV3.bahanTelur() + rtv1.bahanTelur()+ rtv2.bahanTelur()+ pv1.bahanTelur());
         double hitungEs = Math.ceil(rmV1.bahanEs() + rmV2.bahanEs()+ rmV3.bahanEs() + rtv1.bahanEs()+ rtv2.bahanEs()+ pv1.bahanEs());
         
-        
+        //Total Kemasan Bahan yang dibutuhkan
         double hitungKemasanTerigu = Math.ceil(hitungTerigu/1000);
         double hitungKemasanGula = Math.ceil(hitungGula/1000);
         double hitungKemasanButter = Math.ceil(hitungButter/1000);
@@ -65,7 +64,31 @@ public class Main {
         double hitungKemasanSusuCair = Math.ceil(hitungSusuCair/1000);
         double hitungKemasanTelur = Math.ceil(hitungTelur/1000);
         double hitungKemasanEs = Math.ceil(hitungEs/1000);
+
+        //Total Berat Topping dan Filling yang dibutuhkan
+        double hitungKeju = Math.ceil(rmV1.bahanKeju() + rmV2.bahanKeju() + rmV3.bahanKeju() + rtv1.bahanKeju() + rtv2.bahanKeju() + pv1.bahanKeju());
+        double hitungCoklat = Math.ceil(rmV1.bahanCoklat() + rmV2.bahanCoklat() + rmV3.bahanCoklat() + rtv1.bahanCoklat() + rtv2.bahanCoklat() + pv1.bahanCoklat());
+        double hitungVanila = Math.ceil(rmV1.bahanVanila() + rmV2.bahanVanila() + rmV3.bahanVanila() + rtv1.bahanVanila() + rtv2.bahanVanila() + pv1.bahanVanila());
+        double hitungRedBean = Math.ceil(rmV1.bahanRedBean() + rmV2.bahanRedBean() + rmV3.bahanRedBean() + rtv1.bahanRedBean() + rtv2.bahanRedBean() + pv1.bahanRedBean());
+        double hitungSosis = Math.ceil(rmV1.bahanSosis() + rmV2.bahanSosis() + rmV3.bahanSosis() + rtv1.bahanSosis() + rtv2.bahanSosis() + pv1.bahanSosis());
+        double hitungBeef = Math.ceil(rmV1.bahanBeef() + rmV2.bahanBeef() + rmV3.bahanBeef() + rtv1.bahanBeef() + rtv2.bahanBeef() + pv1.bahanBeef());
+        double hitungBombay = Math.ceil(rmV1.bahanBombay() + rmV2.bahanBombay() + rmV3.bahanBombay() + rtv1.bahanBombay() + rtv2.bahanBombay() + pv1.bahanBombay());
+
+        //Total Kemasan Topping Filling yang dibutuhkan
+        double hitungKemasanKeju = Math.ceil(hitungKeju/1000);
+        double hitungKemasanCoklat = Math.ceil(hitungCoklat/1000);
+        double hitungKemasanVanila = Math.ceil(hitungVanila/1000);
+        double hitungKemasanRedBean = Math.ceil(hitungRedBean/1000);
+        double hitungKemasanSosis = Math.ceil(hitungSosis/1000);
+        double hitungKemasanBeef = Math.ceil(hitungBeef/1000);
+        double hitungKemasanBombay = Math.ceil(hitungBombay/1000);
+
+        System.out.println(" ");
+        System.out.println("===================================");
+        System.out.println("     BAHAN YANG DIBUTUHKAN");
+        System.out.println("===================================");   
         
+        //printout Bahan
         System.out.println("Tepung terigu yang dibutuhkan " + hitungTerigu + " gram" + " membutuhkan: " + hitungKemasanTerigu + " kemasan ");
         System.out.println("Gula yang dibutuhkan " + hitungGula + " gram" + " membutuhkan: " + hitungKemasanGula + " kemasan ");
         System.out.println("Butter yang dibutuhkan " + hitungButter + " gram" + " membutuhkan: " + hitungKemasanButter + " kemasan ");
@@ -75,7 +98,20 @@ public class Main {
         System.out.println("Telur yang dibutuhkan " + hitungTelur + " gram" + " membutuhkan: " + hitungKemasanTelur + " kemasan ");
         System.out.println("Es batu yang dibutuhkan " + hitungEs + " gram" + " membutuhkan: " + hitungKemasanEs + " kemasan ");
 
+        System.out.println(" ");
+        System.out.println("=============================================");
+        System.out.println("     TOPPING DAN FILLING YANG DIBUTUHKAN");
+        System.out.println("=============================================");   
 
-         
+        //printout Topping Filling
+        System.out.println("Keju terigu yang dibutuhkan " + hitungKeju + " gram" + " membutuhkan: " + hitungKemasanKeju + " kemasan ");
+        System.out.println("Coklat yang dibutuhkan " + hitungCoklat + " gram" + " membutuhkan: " + hitungKemasanCoklat + " kemasan ");
+        System.out.println("Vanila yang dibutuhkan " + hitungVanila + " gram" + " membutuhkan: " + hitungKemasanVanila + " kemasan ");
+        System.out.println("RedBean yang dibutuhkan " + hitungRedBean + " gram" + " membutuhkan: " + hitungKemasanRedBean + " kemasan ");
+        System.out.println("Sosis yang dibutuhkan " + hitungSosis + " gram" + " membutuhkan: " + hitungKemasanSosis + " kemasan ");
+        System.out.println("Beef yang dibutuhkan " + hitungBeef + " gram" + " membutuhkan: " + hitungKemasanBeef + " kemasan ");
+        System.out.println("Bombay yang dibutuhkan " + hitungBombay + " gram" + " membutuhkan: " + hitungKemasanBombay + " kemasan ");
+        System.out.println(" ");
+
     }
 }
