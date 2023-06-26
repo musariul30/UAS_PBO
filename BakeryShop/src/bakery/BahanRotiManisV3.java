686b9db497c291b6e278d40df1dc210a99afdefc
RuthRadekta <ruthradektal@gmail.com>
2023-06-26 17:17:26 +0700

    Update BahanRotiManisV3

--- BakeryShop/src/bakery/BahanRotiManisV3.java
+++ BakeryShop/src/bakery/BahanRotiManisV3.java
@@ -1,5 +1,92 @@
 package bakery;
 
-public class BahanRotiManisV3 {
+public class BahanRotiManisV3 extends RotiManis implements InterfaceHitungBahan{
+
+    public int orderan;
     
+    @Override
+    public double bahanTerigu() {
+        double hitung = this.teriguPcsRotiManis * orderan;
+        return hitung;
+    }
+
+    @Override
+    public double bahanGula() {
+        double hitung = this.gulaPcsRotiManis * orderan;
+        return hitung;
+    }
+
+    @Override
+    public double bahanButter() {
+        double hitung = this.butterPcsRotiManis * orderan;
+        return hitung;
+    }
+
+    @Override
+    public double bahanRagi() {
+        double hitung = this.ragiPcsRotiManis * orderan;
+        return hitung;
+    }
+
+    @Override
+    public double bahanSusuBubuk() {
+        double hitung = this.susuBubukPcsRotiManis * orderan; 
+        return hitung;        
+    }
+
+    @Override
+    public double bahanSusuCair() {
+        double hitung = this.susuCairPcsRotiManis * orderan;
+        return hitung;
+    }
+
+    @Override
+    public double bahanTelur() {
+        double hitung = this.TelurPcsRotiManis * orderan;
+        return hitung;
+                
+    }
+
+    @Override
+    public double bahanEs() {
+        double hitung = this.esPcsRotiManis * orderan;
+        return hitung;
+    }
+
+    @Override
+    public double bahanKeju() {
+        double hitung = 10 * orderan;
+        return  hitung;
+    }
+
+    @Override
+    public double bahanCoklat() {
+        return 0;
+    }
+
+    @Override
+    public double bahanVanila() {
+        return 0;
+    }
+
+    @Override
+    public double bahanRedBean() {
+        return 0;
+    }
+
+    @Override
+    public double bahanSosis() {
+        double hitung = 10 * orderan;
+        return  hitung;
+    }
+
+    @Override
+    public double bahanBeef() {
+        return 0;
+    }
+
+    @Override
+    public double bahanBombay() {
+        return 0;
+    }
 }
